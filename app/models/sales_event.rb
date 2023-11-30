2bbe8b4b6ca15ae193e6c1c73e822471286c5b0c
Simon Rapilly <85184231+srapilly@users.noreply.github.com>
2023-11-30 08:17:45 -0500

    Active Record callback for low inventory

--- app/models/sales_event.rb
+++ app/models/sales_event.rb
@@ -3,4 +3,15 @@ class SalesEvent < ApplicationRecord
   validates :store, presence: true
   validates :model, presence: true
   validates :inventory, presence: true
+
+
+  after_create :notify_low_inventory
+
+  private
+
+  def notify_low_inventory
+    if inventory < 5
+      # send something (email, webhook, ...)
+    end
+  end
 end
