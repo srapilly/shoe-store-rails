011bc7ae68435ab52bb56c1127bfda233580f986
Simon Rapilly <85184231+srapilly@users.noreply.github.com>
2023-11-30 08:34:55 -0500

    Rubocop setup and CI (#1)

--- db/migrate/20231130031610_create_sales_events.rb
+++ db/migrate/20231130031610_create_sales_events.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateSalesEvents < ActiveRecord::Migration[7.1]
   def change
     create_table :sales_events do |t|
       t.string :store, null: false
-      t.string :model,  null: false
+      t.string :model, null: false
       t.integer :inventory, null: false
 
       t.timestamps
--- test/controllers/sales_events_controller_test.rb
+++ test/controllers/sales_events_controller_test.rb
@@ -1,45 +1,51 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class SalesEventsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @sales_event = sales_events(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get sales_events_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_sales_event_url
     assert_response :success
   end
 
-  test "should create sales_event" do
-    assert_difference("SalesEvent.count") do
-      post sales_events_url, params: { sales_event: { inventory: @sales_event.inventory, model: @sales_event.model, store: @sales_event.store } }
+  test 'should create sales_event' do
+    assert_difference('SalesEvent.count') do
+      post sales_events_url,
+           params: { sales_event: { inventory: @sales_event.inventory, model: @sales_event.model,
+                                    store: @sales_event.store } }
     end
 
     assert_redirected_to sales_event_url(SalesEvent.last)
   end
 
-  test "should show sales_event" do
+  test 'should show sales_event' do
     get sales_event_url(@sales_event)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_sales_event_url(@sales_event)
     assert_response :success
   end
 
-  test "should update sales_event" do
-    patch sales_event_url(@sales_event), params: { sales_event: { inventory: @sales_event.inventory, model: @sales_event.model, store: @sales_event.store } }
+  test 'should update sales_event' do
+    patch sales_event_url(@sales_event),
+          params: { sales_event: { inventory: @sales_event.inventory, model: @sales_event.model,
+                                   store: @sales_event.store } }
     assert_redirected_to sales_event_url(@sales_event)
   end
 
-  test "should destroy sales_event" do
-    assert_difference("SalesEvent.count", -1) do
+  test 'should destroy sales_event' do
+    assert_difference('SalesEvent.count', -1) do
       delete sales_event_url(@sales_event)
     end
 
